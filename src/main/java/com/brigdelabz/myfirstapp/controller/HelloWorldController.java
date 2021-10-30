9844fab240baa1e0f0cf2ac5677d9a064d105db3
Swathi <swathi.h@perfios.com>
2021-10-30 18:40:23 +0530

    ADDED: Get method to show hello message

--- src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
@@ -7,4 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloWorldController {
 
+	@RequestMapping(value = {"", "/", "/home"})
+	public String sayHello() {
+		return "Hello World!!";
+	}
 }
