2024a17bb2a8a5cb9ddfd0c47fd850dfa001b65c
Swathi <swathi.h@perfios.com>
2021-10-30 18:56:25 +0530

    ADDED: REST call to post request and pass name in the body by creating DTO bean

--- src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
@@ -2,27 +2,36 @@ package com.brigdelabz.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brigdelabz.myfirstapp.model.User;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
-	@RequestMapping(value = {"", "/", "/home"})
+	@RequestMapping(value = { "", "/", "/home" })
 	public String sayHello() {
 		return "Hello World!!";
 	}
-	
-	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+
+	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name + "!";
 	}
-	
+
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
+	}
+
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
 	}
 }
