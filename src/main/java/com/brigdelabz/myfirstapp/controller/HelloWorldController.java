e7ad97eeb512916423323e59626a494d175607ae
Swathi <swathi.h@perfios.com>
2021-10-30 18:35:21 +0530

    ADDED: Initializing project structure

---
+++ src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
@@ -0,0 +1,10 @@
+package com.brigdelabz.myfirstapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+}
