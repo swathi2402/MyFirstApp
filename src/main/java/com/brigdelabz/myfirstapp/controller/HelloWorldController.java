54d372f76c0a1e9020bd592dd3468a4eb4080d60
Swathi <swathi.h@perfios.com>
2021-10-30 18:45:51 +0530

    ADDED: REST call to pass name as query parameter

--- src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/brigdelabz/myfirstapp/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.brigdelabz.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloWorldController {
 	@RequestMapping(value = {"", "/", "/home"})
 	public String sayHello() {
 		return "Hello World!!";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
 	}
 }
