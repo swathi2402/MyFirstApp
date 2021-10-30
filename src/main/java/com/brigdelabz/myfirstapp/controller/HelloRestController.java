a7a8eae9af5bbe5445f4a0e12fa95502638ab10d
Swathi <swathi.h@perfios.com>
2021-10-30 19:26:26 +0530

    ADDED: POST Request method to pass name in the body using DTO bean

--- src/main/java/com/brigdelabz/myfirstapp/controller/HelloRestController.java
+++ src/main/java/com/brigdelabz/myfirstapp/controller/HelloRestController.java
@@ -2,10 +2,14 @@ package com.brigdelabz.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brigdelabz.myfirstapp.model.User;
 
 @RestController
 @RequestMapping("/firstapp")
@@ -24,5 +28,10 @@ public class HelloRestController {
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
 	}
 }
